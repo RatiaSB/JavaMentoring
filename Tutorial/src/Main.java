246bc3addd1794dafb9d2f0d5d8bf9cafff8b3d7
RatiaSB <230724477@mycput.ac.za>
2024-04-21 23:01:20 +0200

    Added try catch

--- Tutorial/src/Main.java
+++ Tutorial/src/Main.java
@@ -1,11 +1,24 @@
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
+// Try exception
+        try{
+            //statement allows you to define a block of code to be tested for errors while it is being executed.
+        String[] arr = new String[9];
 
-        String[] arr = new String[3];
-
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 11; i++){
             System.out.println("index is" + arr[i] );
+
         }
-    }
+
+    } catch (ArrayIndexOutOfBoundsException ex){
+            // code to be executed if error occurs
+           // System.out.println("Please enter value from 0 to 9 ");
+            JOptionPane.showMessageDialog(null,"array only have 9 index \n"+ ex,"ERROR",JOptionPane.ERROR_MESSAGE);
+        }
+
+        }
 
 }
