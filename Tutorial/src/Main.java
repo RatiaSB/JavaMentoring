b4250d2d91e1112fe2b0990419339e181bb8c983
RatiaSB <230724477@mycput.ac.za>
2024-04-21 22:21:52 +0200

    code to demonstrate try catch

---
+++ Tutorial/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] arr = new String[3];
+
+        for(int i = 0; i < 4; i++){
+            System.out.println("index is" + arr[i] );
+        }
+    }
+
+}
